a0fa585c221b5206768d37c0510f21e7e68e4a29
Rafael Alonso <rafael.palonso@gmail.com>
2017-11-07 10:18:22 -0500

    Made user omniauthable

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  devise :omniauthable, omniauth_providers: [:facebook]
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email,
