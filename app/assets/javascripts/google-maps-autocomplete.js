09db3e0b402846c85a846452b45f56dec88750a1
Robin Handous <robin.handous@gmail.com>
2017-11-08 11:16:57 -0500

    added autocomplete on the new meal view

---
+++ app/assets/javascripts/google-maps-autocomplete.js
@@ -0,0 +1,72 @@
+function onPlaceChanged() {
+  var place = this.getPlace();
+  var components = getAddressComponents(place);
+
+  var mealAddress = document.getElementById('meal_address');
+  mealAddress.blur();
+  mealAddress.value = components.address;
+
+  document.getElementById('meal_zip_code').value = components.zip_code;
+  document.getElementById('meal_city').value = components.city;
+
+  if (components.country_code) {
+    var selector = '#meal_country option[value="' + components.country_code + '"]';
+    document.querySelector(selector).selected = true;
+  }
+}
+
+function getAddressComponents(place) {
+  // If you want lat/lng, you can look at:
+  // - place.geometry.location.lat()
+  // - place.geometry.location.lng()
+
+  if (window.console && typeof console.log === "function") {
+    console.log(place);
+  }
+
+  var street_number = null;
+  var route = null;
+  var zip_code = null;
+  var city = null;
+  var country_code = null;
+  for (var i in place.address_components) {
+    var component = place.address_components[i];
+    for (var j in component.types) {
+      var type = component.types[j];
+      if (type === 'street_number') {
+        street_number = component.long_name;
+      } else if (type === 'route') {
+        route = component.long_name;
+      } else if (type === 'postal_code') {
+        zip_code = component.long_name;
+      } else if (type === 'locality') {
+        city = component.long_name;
+      } else if (type === 'postal_town' && city === null) {
+        city = component.long_name;
+      } else if (type === 'country') {
+        country_code = component.short_name;
+      }
+    }
+  }
+
+  return {
+    address: street_number === null ? route : (street_number + ' ' + route),
+    zip_code: zip_code,
+    city: city,
+    country_code: country_code
+  };
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  var mealAddress = document.getElementById('meal_address');
+
+  if (mealAddress) {
+    var autocomplete = new google.maps.places.Autocomplete(mealAddress, { types: ['geocode'] });
+    google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
+    google.maps.event.addDomListener(mealAddress, 'keydown', function(e) {
+      if (e.key === "Enter") {
+        e.preventDefault(); // Do not submit the form on Enter.
+      }
+    });
+  }
+});
